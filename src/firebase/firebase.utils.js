45acaf08018355647bab12ce5e47a2933838797a
Alexander <Snakeage@gmail.com>
2020-04-22 18:16:09 +0300

    finishing sign in handleSubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,33 +3,34 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyDAuwLXdu6pYwQhLBUoEagOToy1PPRt0no',
-  authDomain: 'kazan-wear.firebaseapp.com',
-  databaseURL: 'https://kazan-wear.firebaseio.com',
-  projectId: 'kazan-wear',
-  storageBucket: 'kazan-wear.appspot.com',
-  messagingSenderId: '1016604607395',
-  appId: '1:1016604607395:web:ae397ed1aae88f7df6d016',
-  measurementId: 'G-B7CDJLDJLG',
+  apiKey: "AIzaSyDAuwLXdu6pYwQhLBUoEagOToy1PPRt0no",
+  authDomain: "kazan-wear.firebaseapp.com",
+  databaseURL: "https://kazan-wear.firebaseio.com",
+  projectId: "kazan-wear",
+  storageBucket: "kazan-wear.appspot.com",
+  messagingSenderId: "1016604607395",
+  appId: "1:1016604607395:web:ae397ed1aae88f7df6d016",
+  measurementId: "G-B7CDJLDJLG"
 };
+
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc(`user/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
 
-  if (!snapShot.exist) {
+  if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-
     try {
       await userRef.set({
         displayName,
         email,
         createdAt,
-        ...additionalData,
+        ...additionalData
       });
     } catch (error) {
       console.log('error creating user', error.message);
@@ -38,8 +39,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   return userRef;
 };
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
