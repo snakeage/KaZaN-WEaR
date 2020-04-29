26850ce00e5956cb5f02b866832a9b23b4e98143
Alexander <Snakeage@gmail.com>
2020-04-29 20:28:53 +0300

    removing redux-logger from prodaction build

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from 'redux-logger';
 
 import rootreducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootreducer, applyMiddleware(...middlewares));
 
